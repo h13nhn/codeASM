454f390a0d25f810eb15c774d0de53629b51842d
Hien Ngoc Hoang <86940873+h13nhn@users.noreply.github.com>
2023-06-13 17:00:30 +0700

    Update 6.asm

--- 6.asm
+++ 6.asm
@@ -1,54 +1,40 @@
 dseg segment
     tbao0 db 'Nhap ky tu: $'
-    tbao1 db 10,13,'Ky tu HOA!$'
-    tbao2 db 10,13,'Ky tu thuong!$'
-    tbao3 db 10,13,'Ky tu khac!$'
 dseg ends
-
 cseg segment
     assume cs: cseg, ds: dseg
-    start:                       
-    mov ax, dseg 
+start:   
+    mov ax, dseg
     mov ds, ax
     
     mov ah, 09h
     lea dx, tbao0
     int 21h
     
-    mov ah, 01h
+    mov ah, 07h
+    int 21h 
+
+    
+    nhan:
+    mov dl, al   
+    push dx
+    
+    mov dl, ' '
+    mov ah, 02h
     int 21h
     
-    cmp al, 'A' 
-    jb khac 
-    cmp al, 'z' 
-    ja khac  
-    
-    cmp al, 'Z'
-    jbe hoa
-    cmp al, 'a'
-    jae thuong
-    
-    jmp khac
-    
-    hoa:
-    mov ah, 09h
-    lea dx, tbao1
+    pop dx
+    mov ah, 02h
     int 21h
-    jmp exit
     
-    thuong:
-    mov ah, 09h
-    lea dx, tbao2
-    int 21h
-    jmp exit
+    inc al
+    cmp al, 'z'
+    jbe nhan
     
-    khac:
-    mov ah, 09h
-    lea dx, tbao3
-    int 21h
-            
-    exit:
     mov ah, 4Ch
-    int 21h            
+    int 21h
+    
+    
+
 cseg ends
 end start
